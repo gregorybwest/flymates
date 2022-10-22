f10274a706286e68b137d5002738927607820f30
greg <gregorybwest@gmail.com>
2022-10-21 17:23:41 -0700

    comments added to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Airport.destroy_all
-
 Airport.create({name: "Los Angeles International Airport", code: "LAX", zip_code: "90045", city: "Los Angeles", address: "1 World Way"})
 
 Airport.create({name: "Portland International Airport", code: "PDX", zip_code: "97218", city: "Portland", address: "7000 NE Airport Way"})
@@ -20,3 +18,8 @@ User.create({email: "atlanticaviationlax@test.com", password_digest: "password",
 
 User.create({email: "atlanticaviationpdx@test.com", password_digest: "password", user_name: "Atlantic Aviation PDX", is_business: true, airport_id: 2, address: "8089 NE Airport Way, Portland, OR 97218", hours_of_operation: "24 hours", phone_number: "(503) 331-4220", contact_radio: "unknown", service_fuel: true, has_car: true, service_facilities: true, winter_services: true, other_services: true, rating: 0})
 
+Comment.create({user_id: 1, business_id: 3, body: "Great service, friendly staff!", rating: 5})
+
+Comment.create({user_id: 2, business_id: 3, body: "Very professional staff, well maintained!", rating: 4})
+
+Comment.create({user_id: 2, business_id: 4, body: "Good coordiation team to work with.", rating: 5})
