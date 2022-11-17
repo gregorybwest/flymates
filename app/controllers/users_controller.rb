6c86e1eb1c75eaa03ac547e244f4326008c32287
greg <gregorybwest@gmail.com>
2022-11-17 14:29:55 -0800

    user signup and password hashing with bcrypt gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      email: params[:email], 
+      password: params[:password], 
+      password_confirmation: params[:password_confirmation], 
+      user_name: params[:user_name], 
+      is_business: params[:is_business], 
+      airport_id: params[:airport_id], 
+      address: params[:address], 
+      hours_of_operation: params[:hours_of_operation], 
+      phone_number: params[:phone_number], 
+      contact_radio: params[:contact_radio], 
+      service_fuel: params[:service_fuel], 
+      has_car: params[:has_car], 
+      service_facilities: params[:service_facilities], 
+      winter_services: params[:winter_services], 
+      other_services: params[:other_services], 
+      rating: params[:rating]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :is_business, presence: true
   belongs_to :airport, optional: true
   has_many :reviews
+  has_one_attached :image
 
   def is_business?
     self.is_business == true
