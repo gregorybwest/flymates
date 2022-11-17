4cb458f7f61a3e86cd3bed449effcf51063a73b6
greg <gregorybwest@gmail.com>
2022-11-17 11:13:46 -0800

    changes name of comment model to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_221129) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_190240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_221129) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
+  create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
     t.integer "business_id"
     t.text "body"
