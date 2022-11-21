0aa62ca6f7b903f7270081200b0b51bb8ba0f245
greg <gregorybwest@gmail.com>
2022-11-21 15:29:00 -0800

    inclusion validation for is_business

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :user_name, presence: true, uniqueness: true
-  validates :is_business, presence: true
+  validates :is_business, inclusion: { in: [ true, false ] }
   belongs_to :airport, optional: true
   has_many :reviews
   has_one_attached :image
