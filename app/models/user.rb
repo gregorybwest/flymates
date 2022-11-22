a40648eba9c1737e6a399e7e55c9afb946a34e38
greg <gregorybwest@gmail.com>
2022-11-22 14:23:34 -0800

    dependent destroy for user comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :user_name, presence: true, uniqueness: true
   validates :is_business, inclusion: { in: [ true, false ] }
   belongs_to :airport, optional: true
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one_attached :image
 
   def is_business?
