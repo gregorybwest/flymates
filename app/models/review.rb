69f2dd881fd2f0446a6a96ab49b6a141edab6eab
greg <gregorybwest@gmail.com>
2022-11-22 16:56:39 -0800

    review can belong to business and author, both from User class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :business, class_name: "User", foreign_key: "business_id"
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
 end
