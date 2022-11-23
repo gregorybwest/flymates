1170d2d0994832fa07e2260c6ea7d8c8bd664448
greg <gregorybwest@gmail.com>
2022-11-22 17:02:47 -0800

    user has many written_reviews or received_reviews depending if user is business or customer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
+  belongs_to :airport, optional: true
+  # these are reviews the user has left about businesses
+  has_many :written_reviews, dependent: :destroy, foreign_key: :user_id, class_name: "Review"
+  # these are reviews about the business
+  has_many :received_reviews, dependent: :destroy, foreign_key: :business_id, class_name: "Review"
+  has_one_attached :image
+
   validates :email, presence: true, uniqueness: true
   validates :user_name, presence: true, uniqueness: true
   validates :is_business, inclusion: { in: [ true, false ] }
-  belongs_to :airport, optional: true
-  has_many :reviews, dependent: :destroy
-  has_one_attached :image
 
   def is_business?
     self.is_business == true
