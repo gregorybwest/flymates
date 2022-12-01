0e1e89840b3543e4fed905225283a8faf573fa8a
greg <gregorybwest@gmail.com>
2022-12-01 12:38:35 -0800

    reviews create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,20 @@ class ReviewsController < ApplicationController
     render json: reviews
   end
 
+  def create
+    review = Review.new(
+      user_id: current_user.id,
+      business_id: @user.id,
+      body: params[:body],
+      rating: params[:rating]
+    )
+    if review.save
+      render json: review, status: :created
+    else
+      render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def show
     review = @user.received_reviews.find(params[:id])
     render json: review
