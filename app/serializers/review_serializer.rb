9538d4e92fba1e106ca8ae44d134289894721c54
greg <gregorybwest@gmail.com>
2022-12-15 13:28:16 -0800

    adds created_at and replaces :author_name with :author

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :business_id, :body, :rating, :author_name
+  attributes :id, :user_id, :business_id, :body, :rating, :author, :created_at
 end
