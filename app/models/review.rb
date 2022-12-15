bd26d86019ad6a30fca75616ec64641576455b9e
greg <gregorybwest@gmail.com>
2022-12-15 13:26:49 -0800

    remove 'author_name' method

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,4 @@ class Review < ApplicationRecord
   belongs_to :business, class_name: "User", foreign_key: "business_id"
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
-  def author_name
-    User.find(self.user_id).user_name
-  end
-
 end
